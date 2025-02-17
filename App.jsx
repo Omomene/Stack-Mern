44216e9e2c8a71706a5898a84aea21cfcc743cdd
Omomene Iwelomen <iwelomene@gmail.com>
2025-02-17 13:58:29 +0100

    Initial commit

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Cards from './components/cards';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Cards />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
---
+++ components/Cards.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Cards.css';
+
+const Cards = () => {
+  return (
+    <div className="card">
+      <h1>Bonjour Ippsi</h1>
+    </div>
+  );
+};
+
+export default Cards;
